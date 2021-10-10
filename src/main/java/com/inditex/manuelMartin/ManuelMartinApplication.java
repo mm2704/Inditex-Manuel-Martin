eba330271318b6f0a04d4d4185da5cd308d9273c
Manu <martinmanuel2704@gmail.com>
2021-10-10 13:24:59 -0300

    init commit

---
+++ src/main/java/com/inditex/manuelMartin/ManuelMartinApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.manuelMartin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuelMartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuelMartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/manuelMartin/ManuelMartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.manuelMartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuelMartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
